4f587c15aaa3b1abd460b61a1d20ad892590e242
Jose Luis Franconetti Olmedo <joseluis.franconetti@gmail.com>
2020-03-24 16:10:31 +0100

    feat(cd): modified deployment script and configured drone.io

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,14 +4,14 @@
 DOCKER_IMAGE_TAG="$(git rev-parse --short HEAD)"
 
 # first prepare build the image
-docker build -t joselufo/harmony-reference:$DOCKER_IMAGE_TAG -f ../Dockerfile ../.
+docker build -t joselufo/harmony-reference:"$DOCKER_IMAGE_TAG" -f ../Dockerfile ../.
 
 # copying and adding the new docker image tag into the enviroment file
 echo "HARMONY_REFERENCE_VERSION=${DOCKER_IMAGE_TAG}" > ../docker/.env
 
-cat password.txt | docker login --username joselufo --password-stdin
+docker login registry.mobilejazz.com  -u "$DOCKER_REGISTRY_USER" -p "$DOCKER_REGISTRY_PASSWORD" || exit
 
-docker push joselufo/harmony-reference:$DOCKER_IMAGE_TAG
+docker push joselufo/harmony-reference:"$DOCKER_IMAGE_TAG"
 
 # copying all the require files to run the application into the server
-scp ../docker/compose.yml ../docker/compose.prod.yml up.sh ../docker/.env password.txt root@"${SERVER_ADDRESS}":~/app/
+scp ../docker/compose.yml ../docker/compose.prod.yml up.sh ../docker/.env root@"${SERVER_ADDRESS}":~/app/
