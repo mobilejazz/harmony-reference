7f474cf11b8d175c9a41370f66ec6a678140f9f7
Javi G <javier.tb@outlook.com>
2020-04-03 11:51:28 +0200

    fix card href

--- src/components/link-card.js
+++ src/components/link-card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function LinkCard(props) {
   return (
-    <a class="link-card" href="{props.href}">
+    <a class="link-card" href={props.href}>
       <div class="link-card__content">
           <h3 class="link-card__title">{props.title}</h3>
           <p class="link-card__description">{props.description}</p>
