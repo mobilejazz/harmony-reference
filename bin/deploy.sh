ed3299ddb4d0023e6f0950255a99649b5e539588
Jose Luis Franconetti Olmedo <joseluis.franconetti@gmail.com>
2021-03-22 10:03:12 +0100

    chore(drone): removed build temporarily

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,14 +6,14 @@ DOCKER_IMAGE_TAG="$(git rev-parse --short HEAD)"
 # . ./env-vars-deploy.sh
 
 # first prepare build the image
-docker build -t registry.mobilejazz.com/harmony/documentation:"$DOCKER_IMAGE_TAG" -f ../Dockerfile ../.
+# docker build -t registry.mobilejazz.com/harmony/documentation:"$DOCKER_IMAGE_TAG" -f ../Dockerfile ../.
 
 # copying and adding the new docker image tag into the enviroment file
-echo "HARMONY_REFERENCE_VERSION=${DOCKER_IMAGE_TAG}" > ../docker/.env
+# echo "HARMONY_REFERENCE_VERSION=${DOCKER_IMAGE_TAG}" > ../docker/.env
 
-docker login --username "$DOCKER_REGISTRY_USER" -p "$DOCKER_REGISTRY_PASSWORD" registry.mobilejazz.com || exit
+# docker login --username "$DOCKER_REGISTRY_USER" -p "$DOCKER_REGISTRY_PASSWORD" registry.mobilejazz.com || exit
 
-docker push registry.mobilejazz.com/harmony/documentation:"$DOCKER_IMAGE_TAG"
+# docker push registry.mobilejazz.com/harmony/documentation:"$DOCKER_IMAGE_TAG"
 
 echo "$SERVER_USER"@"$SERVER_ADDRESS":"$SERVICE_PATH"
 
