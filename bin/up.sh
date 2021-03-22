728c4e21e78601c7ccf8f4354943c83250edf317
Jose Luis Franconetti Olmedo <joseluis.franconetti@gmail.com>
2021-03-22 11:01:49 +0100

    chore(drone): removed unnecessary code in up.sh script

--- bin/up.sh
+++ bin/up.sh
@@ -5,9 +5,6 @@ source env-vars-pull.sh
 # Log in into the gitlab container register to push the docker image built
 docker login registry.mobilejazz.com  --username "$DOCKER_REGISTRY_USER" --password "$DOCKER_REGISTRY_PASSWORD" || exit
 
-# merge .env-prod variables into the default .env file
-cat .env-prod >> .env
-
 # execute docker-compose command with all the compose files to merge it
 docker-compose -f compose.prod.yml pull
 docker-compose -f compose.prod.yml up -d
